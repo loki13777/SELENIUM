f9837fac094aabe961d02f6b19c3bc8aa423ecd9
loki13777 <polo13123456@mail.ru>
2023-04-09 20:22:30 +0300

    Initial commit

---
+++ chromedriver/constants.py
@@ -0,0 +1,7 @@
+# здесь хранятся переменные проекта
+
+url = 'https://www.21vek.by'
+css_accept = 'button.Button-module__button.Button-module__blue-primary'
+xpath_catalog = "// span[text() = 'Каталог товаров']"
+css_start_categories = 'span.styles_categoryName__28yR1.styles_categoryName__ZUtLQ'
+css_subcategories = 'a.cloud-sub__header'
---
+++ chromedriver/main.py
@@ -0,0 +1,43 @@
+import json
+from constants import *
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome(executable_path='chromedriver')
+
+with driver:
+    # открываем страницу
+    driver.get(url)
+    # находим и нажимаем кнопку принять (найстроки coocies)
+    driver.find_element('css selector', css_accept).click()
+    time.sleep(1)
+    # находим и нажимаем кнопку каталог
+    driver.find_element('xpath', xpath_catalog).click()
+    time.sleep(1)
+    # находим все стартовые категории
+    start_categories = driver.find_elements('css selector', css_start_categories)
+    # записываем названия категорий в список
+    list_start_categories = [el.text for el in start_categories]
+    # создаём словарь для записи результата
+    dict_results = {}
+    # проходимя списком по именам категорий
+    for str_start_category in list_start_categories:
+        # создаём словарь для подкатегорий
+        dict_subcategories = {}
+        time.sleep(3)
+        # находим и нажимаем кнопку категории подставляя её имя в xpath
+        driver.find_element('xpath', f"// span[text() = '{str_start_category}']").click()
+        time.sleep(3)
+        # находим все подкатегории на странице категории
+        subcategories = driver.find_elements('css selector', css_subcategories)
+        for subcategory in subcategories:
+            # записываем в словарь ключём название подкатегории а значением ссылку на эту подкатегорию
+            dict_subcategories[subcategory.text] = subcategory.get_attribute('href')
+        # добавляем в результирующий словарь ключём название категории а значением словарь с подкатегориями
+        dict_results[str_start_category] = dict_subcategories
+        # перезаписываем результирующий словарь в файл
+        with open("../../PARSERS_SCRAPY/project2/project2/spiders/catalog_dict.json", "w", encoding='UTF-8') as file:
+            json.dump(dict_results, file, indent=1, ensure_ascii=False)
+        # нажимаем кнопку каталог перед следующей итерацией цикла
+        driver.find_element('xpath', xpath_catalog).click()
+        time.sleep(3)
